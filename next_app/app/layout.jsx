11ec1f358a318d2281c8a9e29959ff1c5902b029
maripaba2 <jungharerishi@gmail.com>
2024-01-23 22:07:22 +0530

    added navbar and google auth

---
+++ next_app/app/layout.jsx
@@ -0,0 +1,28 @@
+import "../styles/global.css";
+
+import Navbar from "@/components/Navbar";
+import Provider from "@/components/Provider";
+
+export const metadata = {
+  title: "Faculty Flow",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      <Provider>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Navbar />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
---
+++ next_app/app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () =>
+ (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Welcome to
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'>Faculty Flow</span>
+    </h1>
+    <p className='desc text-center'>
+    {/* Faculty Flow is a tool for modern world to  */}
+    </p>
+
+  </section>
+);
+
+export default Home;
